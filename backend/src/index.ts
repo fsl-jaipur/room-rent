82ec69145bf08faa40450d217eb1ac1f4df67c2f
dheeraj1566 <dheerajjangid013@gmail.com>
2026-05-07 13:59:29 +0530

    swagger added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: env.CLIENT_URL,
+    origin: env.CLIENT_URL || "https://roombaazi.com",
     credentials: true,
   })
 );
